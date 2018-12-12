a025b6c83a924e0fc229aa0d08f42f6c1f7caa44
zwd <zwd321081@gmail.com>
2018-12-12 14:55:07 +0800

    feat: add prettier

---
+++ readme.md
@@ -0,0 +1,7 @@
+https://github.com/liangklfangl/astexample
+https://github.com/jamiebuilds/babel-handbook/blob/master/translations/en/plugin-handbook.md#toc-get-the-path-of-a-sub-node
+
+访问方式
+
+1. visitor
+2. get path.get('body')
--- src/nej2es6visitor.js
+++ src/nej2es6visitor.js
@@ -1,44 +1,40 @@
-import generate from '@babel/generator';
-import babel from '@babel/core';
-import template from '@babel/template';
-import * as t from '@babel/types';
-// var babel = require('@babel/core');
-// var template = require("@babel/template");
-// debugger;
+import generate from "@babel/generator";
+import babel from "@babel/core";
+import template from "@babel/template";
+import * as t from "@babel/types";
 
-
-module.exports = function (babel) {
-
+module.exports = function(babel) {
+  // visitor模式无法返回,所以用了一个参数保留。
   let importList = [];
+  let fnbody;
   const buildImport = template.smart(`var IMPORT_NAME = require(SOURCE); `);
 
-  const v1 = {
+  const importVisitor = {
     Identifier(path) {
-      if (path.node.name == 'define') {
+      if (path.node.name == "define") {
         let defineList = path.parent.arguments[0].elements;
         let defineFnParams = path.parent.arguments[1].params;
-        // path.node.name = path.node.name.split('').reverse().join('');
         for (var i = 0, len = defineList.length; i < len; i++) {
           var imp = buildImport({
             IMPORT_NAME: t.identifier(defineFnParams[i].name), //为什么是identifier,因为ast解析出来就是这个类型。
-            SOURCE: t.stringLiteral(defineList[i].value),
+            SOURCE: t.stringLiteral(defineList[i].value)
           });
 
           importList.push(imp);
-          // path.insertBefore(imp);
-          // console.log(generate(imp).code)
         }
-        // visitor模式无法返回。
-        // return arr;
+
+        fnbody = path.parent.arguments[1].body.body;
       }
-    },
+    }
   };
   return {
     visitor: {
       Program(path, state) {
-        path.traverse(v1);
-        path.pushContainer('body', importList);
-      },
-    },
+        path.traverse(importVisitor);
+        path.node.body = []; //清空body
+        path.pushContainer("body", importList);
+        path.pushContainer("body", fnbody);
+      }
+    }
   };
 };
